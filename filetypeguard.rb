bec876f664aff37cd4c10e4e4b4e77e7302061d6
Yibie <gunshotbox@gmail.com>
2026-02-26 08:23:31 +0800

    chore: update cask formula for v1.2.0

--- filetypeguard.rb
+++ filetypeguard.rb
@@ -1,6 +1,6 @@
 cask "filetypeguard" do
-  version "1.1"
-  sha256 "afffef2fc04a5d214f88d2dd0af2c8aa90eebe26b9b8f0b1376799c5693ccc0c"
+  version "1.2.0"
+  sha256 "cbd3026d28389b057aa54207b390a35c34e857fbb1b850bc2f4621080246c094"
 
   url "https://github.com/yibie/FileTypeGuard/releases/download/v#{version}/FileTypeGuard-#{version}.zip"
   name "FileTypeGuard"
